1806740739e3d1e95b32374cd446eaf8cee2397f
nakajima-ittoku <nakajima.web.tips@gmail.com>
2024-02-13 09:46:52 +0900

    chore: finish astro tutorial

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,20 @@
+import { useState } from "preact/hooks";
+
+export default function Greetin({
+  messages
+}) {
+  const randomMessage = () => messages[(Math.floor(
+    Math.random() * messages.length
+  ))]
+
+  const [greeting, setGreeting] = useState(messages[0])
+
+  return (
+    <div>
+      <h3>{greeting}! 訪問いただきありがとうございます!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>
+        新しい挨拶
+      </button>
+    </div>
+  )
+}
